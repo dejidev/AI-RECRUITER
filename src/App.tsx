fbe9ecf9d6468e9a29e30a2a99bf46a9119952f7
ayodeji <adetayoayodeji7@gmail.com>
2025-05-12 00:57:46 +0100

    Setting up styling and environment

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+
+  return (
+    <>
+      <div className="flex items-center justify-center h-screen">
+        <div className="bg-red-700 text-6xl text-red-100 p-8 rounded-lg shadow-lg">
+          AI RECRUITER
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
